c76556045e2a53a4af964907d88655c9ed5ac510
Big_Earl <477117@chrome.escg.ac.uk>
2019-10-22 15:30:19 +0100

    i have created all the files in which i will place my assets in. I have also placed my main character in the game and have watched the first tutuorial video. I have also removed the template background from the tutorial and place level ones background

---
+++ Skeletal Steve/Assets/Spooky Steve/SteveMoving.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteveMoving : MonoBehaviour
+{
+
+    public int playerSpeed = 5;
+    public bool facingRight = true;
+    public int playerJumpPower = 300;
+    public float moveX;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+        //CONTROLS
+        moveX = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+        
+        if (moveX < 0.0f && facingRight == false)
+        {
+            FlipPlayer();
+        }
+        else if (moveX > 0.0f && facingRight == true)
+        {
+            FlipPlayer();
+        }
+        //PHYSICS
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void Jump()
+    {
+        //JUMPING CODE
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
+    }
+
+    void FlipPlayer()
+    {
+        facingRight = !facingRight;
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+
+}
