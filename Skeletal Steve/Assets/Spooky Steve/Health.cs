999a589f19fa6b398f1d78584cf9d43a68be9b65
Big_Earl <477117@chrome.escg.ac.uk>
2019-11-04 15:30:45 +0000

    Enabled the character to respawn if the player falls under the map (1st stage of death/life mechanic)

---
+++ Skeletal Steve/Assets/Spooky Steve/Health.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Health : MonoBehaviour
+{
+    public int health;
+    public bool hasDied;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+     hasDied = false;
+    }
+
+    
+    void Update()
+    {
+       if (gameObject.transform.position.y < -7) {
+            hasDied = true;
+        }
+       if (hasDied == true) {
+            StartCoroutine ("Die");
+        }
+    }
+    IEnumerator Die() {
+        SceneManager.LoadScene("Level 1 forest");
+        yield return null;
+    }
+}
