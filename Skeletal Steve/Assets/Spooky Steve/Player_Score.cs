2c2ff69fc86ebe7816983df6cef0923e866aa79b
Big_Earl <477117@chrome.escg.ac.uk>
2019-11-18 14:24:53 +0000

    I Have Finished The Score and Time System

--- Skeletal Steve/Assets/Spooky Steve/Player_Score.cs
+++ Skeletal Steve/Assets/Spooky Steve/Player_Score.cs
@@ -2,16 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class Player_Score : MonoBehaviour
 {
     private float timeLeft = 120;
     public int playerScore = 0;
-    
+    public GameObject timeLeftUI;
+    public GameObject playerScoreUI;
 
     // Update is called once per frame
     void Update()
     {
         timeLeft -= Time.deltaTime;
+        timeLeftUI.gameObject.GetComponent<Text>().text = ("Time Left: " + (int)timeLeft);
+        playerScoreUI.gameObject.GetComponent<Text>().text = ("Score: " + playerScore);
         if (timeLeft < 0.1f)
         {
             SceneManager.LoadScene("Level 1 forest");
